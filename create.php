ded958e2a88a0c381c1e591572541c99b18235f6
Reo Yamanaka <ryamanaka807@gmail.com>
2021-06-29 10:52:10 -0400

    added feature to store email in db

---
+++ create.php
@@ -0,0 +1,8 @@
+<?php
+    include "../db.php";
+    $email = $_POST["email"];
+    $sql = "INSERT INTO emailList (email, created) VALUES ('$email', NOW())";
+    $conn -> query($sql);
+    $conn -> close();
+    header("location: index.php");
+?>
