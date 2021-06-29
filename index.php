a0d3949cfe918f8f4c0ec0d6a472663dad6b75c1
Reo Yamanaka <ryamanaka807@gmail.com>
2021-06-29 10:20:12 -0400

    created index and form

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>PHP MySQL Email CRUD</h1>
+        <p>Create, read, update and delete email(s) below:</p>
+
+        <form class="form-inline m-2" action="create.php" method="POST">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control m-2" id="email" name="email">
+            <button type="submit" class="btn btn-primary">Add</button>
+        </form>
+    </div>
+</body>
+</html>
